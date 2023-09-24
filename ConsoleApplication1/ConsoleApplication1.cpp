b89aaa32c38098ef166842765f603805b66ebaab
Сергій Ступак <pop.kom.dabl@gmail.com>
2023-09-25 00:10:23 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    //Задача HELLO
+    cout << "HELLO!!!" << endl;
+
+    cout << endl << endl << endl << endl;
+
+    //Задача SQUARE
+    cout << "00000000000" << endl;
+    cout << "0         0" << endl;
+    cout << "0         0" << endl;
+    cout << "0         0" << endl;
+    cout << "0         0" << endl;
+    cout << "00000000000" << endl;
+   
+    cout << endl << endl << endl << endl << endl;
+
+    //Задача ANKETA
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "       Прізвище : Ступак" << endl;
+    cout << "           Ім'я : Сергій" << endl;
+    cout << "    По батькові : Олегович" << endl;
+    cout << "          Стать : чоловіча" << endl;
+    cout << "Дата народження : 15.06.2006" << endl;
+    cout << "         Адреса : м. Дніпро, пр. Гагаріна 28" << endl;
+    cout << " Номер телефону : +380666636554" << endl;
+    cout << " Місце навчання : ФПМ, ДНУ ім. Олеся Гончара" << endl;
+    cout << "          Група : ПА-23-1" << endl;
+    cout << "          Хоббі : програмування, спорт, ігри" << endl;
+    cin.get();
+}
+
